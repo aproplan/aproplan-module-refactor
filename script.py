f56229a966eaa2b552c6fe56a021a30f005e6c05
qluc <luc.quentin@gmail.com>
2019-01-09 10:54:02 +0100

    Add the possibility to update all files recursively

--- script.py
+++ script.py
@@ -49,29 +49,37 @@ def use_imports(fname, path_name, tsconfig_name, tsconfig_alias):
 print("Starting lines deletion...");
 print()
 
-path = "components/treeList/";
-for filename in os.listdir(path):
-    if os.path.isfile(path + filename):
-        file_length = file_len(path + filename)
-        delete_first_last_lines(path + filename, file_length)
-        use_imports(path + filename, "ap.controllers.", "controllers", "ContactController")
-        use_imports(path + filename, "ap.controllers.", "controllers", "AccessRightController")
-        use_imports(path + filename, "ap.controllers.", "controllers", "AuthenticateController")
-        use_imports(path + filename, "ap.controllers.", "controllers", "CompanyController")
-        use_imports(path + filename, "ap.controllers.", "controllers", "ControllersManager")
-        use_imports(path + filename, "ap.controllers.", "controllers", "CustomViewController")
-        use_imports(path + filename, "ap.controllers.", "controllers", "DashboardController")
-        use_imports(path + filename, "ap.controllers.", "controllers", "DocumentController")
-        use_imports(path + filename, "ap.controllers.", "controllers", "EntityUpdatedEvent")
-        use_imports(path + filename, "ap.controllers.", "controllers", "FormController")
-        use_imports(path + filename, "ap.controllers.", "controllers", "ImportExcelController")
-        use_imports(path + filename, "ap.controllers.", "controllers", "ListController")
-        use_imports(path + filename, "ap.controllers.", "controllers", "MainController")
-        use_imports(path + filename, "ap.controllers.", "controllers", "MassExportController")
-        use_imports(path + filename, "ap.controllers.", "controllers", "MeetingController")
-        use_imports(path + filename, "ap.controllers.", "controllers", "NoteController")
-        use_imports(path + filename, "ap.controllers.", "controllers", "ProjectController")
-        use_imports(path + filename, "ap.controllers.", "controllers", "ReportController")
-        use_imports(path + filename, "ap.controllers.", "controllers", "StateParams")
-        use_imports(path + filename, "ap.controllers.", "controllers", "UIStateController")
-        use_imports(path + filename, "ap.controllers.", "controllers", "UserController")
+# for all files recursively
+for root, dirs, files in os.walk(".", topdown=False):
+   for name in files:
+       file_name = os.path.join(root, name)
+       print(file_name)
+       use_imports(file_name, "ap.models.projects.", "models/projects", "NoteProjectStatus", "NoteProjectStatus")
+
+# for files within a specific folder
+# path = "components/treeList/";
+# for filename in os.listdir(path):
+#    if os.path.isfile(path + filename):
+#        file_length = file_len(path + filename)
+#        delete_first_last_lines(path + filename, file_length)
+#        use_imports(path + filename, "ap.controllers.", "controllers", "ContactController")
+#        use_imports(path + filename, "ap.controllers.", "controllers", "AccessRightController")
+#        use_imports(path + filename, "ap.controllers.", "controllers", "AuthenticateController")
+#        use_imports(path + filename, "ap.controllers.", "controllers", "CompanyController")
+#        use_imports(path + filename, "ap.controllers.", "controllers", "ControllersManager")
+#        use_imports(path + filename, "ap.controllers.", "controllers", "CustomViewController")
+#        use_imports(path + filename, "ap.controllers.", "controllers", "DashboardController")
+#        use_imports(path + filename, "ap.controllers.", "controllers", "DocumentController")
+#        use_imports(path + filename, "ap.controllers.", "controllers", "EntityUpdatedEvent")
+#        use_imports(path + filename, "ap.controllers.", "controllers", "FormController")
+#        use_imports(path + filename, "ap.controllers.", "controllers", "ImportExcelController")
+#        use_imports(path + filename, "ap.controllers.", "controllers", "ListController")
+#        use_imports(path + filename, "ap.controllers.", "controllers", "MainController")
+#        use_imports(path + filename, "ap.controllers.", "controllers", "MassExportController")
+#        use_imports(path + filename, "ap.controllers.", "controllers", "MeetingController")
+#        use_imports(path + filename, "ap.controllers.", "controllers", "NoteController")
+#        use_imports(path + filename, "ap.controllers.", "controllers", "ProjectController")
+#        use_imports(path + filename, "ap.controllers.", "controllers", "ReportController")
+#        use_imports(path + filename, "ap.controllers.", "controllers", "StateParams")
+#        use_imports(path + filename, "ap.controllers.", "controllers", "UIStateController")
+#        use_imports(path + filename, "ap.controllers.", "controllers", "UserController")
